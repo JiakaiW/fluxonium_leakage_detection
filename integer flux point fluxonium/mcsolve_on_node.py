da3a932862cef561acd981dd83d70d4dc8e61579
JiakaiW <142082510+JiakaiW@users.noreply.github.com>
2023-08-30 23:30:11 -0500

    edited mcsolve on node

--- integer flux point fluxonium/mcsolve_on_node.py
+++ integer flux point fluxonium/mcsolve_on_node.py
@@ -1,25 +1,52 @@
 import qutip
 import pickle
+import numpy as np
+import zipfile
+import gzip
+
+amp = 0.002
+
+w_d = 2.9951027102366083
 
 
+def square_cos(t,*args):
+    cos = np.cos(w_d * 2*np.pi * t)
+    return  2*np.pi * amp * cos
 
 class packed_mcsolve_problem:
-    def __init__(self, H, state0, tlist, options, c_ops,ntraj):
+    def __init__(self, H, state0, tlist, chunk_seeds, c_ops):
         self.H = H
         self.state0 = state0
         self.tlist = tlist
-        self.options = options
+        self.chunk_seeds = chunk_seeds
         self.c_ops = c_ops
-        self.ntraj = ntraj
     def run_mcsolve(self):
         result = qutip.mcsolve(
             H=self.H,
             psi0=self.state0,
             tlist=self.tlist,
-            options=self.options,
+            options=qutip.Options(store_states=True, nsteps=2000, num_cpus=len(self.chunk_seeds),seeds=self.chunk_seeds),
             c_ops=self.c_ops,
-            ntraj=self.ntraj
+            ntraj=len(self.chunk_seeds),
+            progress_bar=False
         )
+
+        # Add an attribute to store the number of trajectories before averaging
+        result.num_trajectories_before_averaging = len(self.chunk_seeds)
+
+        # Convert the list of Qobj states to a NumPy array for efficient averaging
+        states_array = np.array([[state.full() for state in traj] for traj in result.states])
+
+        # Average over the trajectories (axis 0 is the trajectory axis)
+        averaged_states_array = np.mean(states_array, axis=0)
+
+        # Convert the averaged states back to Qobj
+        averaged_states = [qutip.Qobj(state) for state in averaged_states_array]
+
+        # Replace the states in the result object with the averaged states
+        result.states = averaged_states
+
+
         return result
 
 def main(idx):
@@ -30,9 +57,12 @@ def main(idx):
     # Run mcsolve
     result = problem.run_mcsolve()
 
-    # Save the result to another .pkl file
-    with open(f"result_{idx}.pkl", "wb") as f:
-        pickle.dump(result, f)
+    # This is essentially keeping only one out of four timeslices to reduce size
+    result.states = result.states[::4]
+    result.times = result.times[::4]
+
+    with gzip.GzipFile(fileobj=sys.stdout.buffer, mode='wb') as f_out:
+        pickle.dump(result, f_out)
 
 if __name__ == "__main__":
     import sys
