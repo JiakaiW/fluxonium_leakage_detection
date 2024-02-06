31410d93fcc5dee9148ad520b6c9cb926978b366
JiakaiW <142082510+JiakaiW@users.noreply.github.com>
2024-02-06 13:14:01 -0600

    feb 6 start looking at critical pn

--- integer flux point fluxonium/plotting/temp_functions.py
+++
@@ -1,9 +0,0 @@
-
-from scqubits import Fluxonium
-qubit_level = 25
-def compute_z(params):
-    EC, EL, EJ = params
-    qbt = Fluxonium(EJ=EJ, EC=EC, EL=EL, flux=0, cutoff=110, truncated_dim=qubit_level)
-    num_evals = 3
-    evals = qbt.eigenvals(num_evals)
-    return evals[2] - evals[1]
